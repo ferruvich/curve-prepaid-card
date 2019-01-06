d5369aa8b04e6af81a7e481a90ae0416cb918deb
ferruvich <ferruvich@gmail.com>
2019-01-06 15:53:50 +0100

    Completed unit testing pkg/psql

--- pkg/psql/pipeline_test.go
+++ pkg/psql/pipeline_test.go
@@ -3,8 +3,24 @@ package psql
 import (
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
+
+// fakeSQLResult implements sql.Result in order to make us able to
+// mock Transaction functions such as Exec
+type fakeSQLresult struct{}
+
+// Since we do not care what these functions are returning
+// we make them as simple as possible
+func (f *fakeSQLresult) LastInsertId() (int64, error) {
+	return 1, nil
+}
+
+func (f *fakeSQLresult) RowsAffected() (int64, error) {
+	return 1, nil
+}
 
 func TestNewPipelineStmt(t *testing.T) {
 
@@ -31,9 +47,64 @@ func TestNewPipelineStmt(t *testing.T) {
 }
 
 func TestPipelineStmt_Exec(t *testing.T) {
-	// TODO
+	pipelineStmt := &PipelineStmt{
+		query: "SQL INSERT Query", args: []interface{}{},
+	}
+
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	mockTransaction := NewMockTransaction(controller)
+
+	mockTransaction.EXPECT().Exec("SQL INSERT Query").Return(
+		&fakeSQLresult{}, nil,
+	)
+
+	sqlResult, err := pipelineStmt.Exec(mockTransaction)
+
+	require.NoError(t, err)
+	require.NotNil(t, sqlResult)
 }
 
 func TestRunPipeline(t *testing.T) {
-	// TODO
+
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	mockTransaction := NewMockTransaction(controller)
+
+	mockTransaction.EXPECT().Exec("Q1").Return(
+		&fakeSQLresult{}, nil,
+	)
+	mockTransaction.EXPECT().Exec("Q2").Return(
+		nil, errors.Errorf("Error"),
+	)
+
+	tests := map[string]struct {
+		pipelineStmt   *PipelineStmt
+		expectingError bool
+	}{
+		"it should fail executing query": {
+			pipelineStmt: &PipelineStmt{
+				query: "Q2",
+			}, expectingError: true,
+		},
+		"it should not fail": {
+			pipelineStmt: &PipelineStmt{
+				query: "Q1",
+			}, expectingError: false,
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			res, err := RunPipeline(mockTransaction, test.pipelineStmt)
+
+			if test.expectingError {
+				require.Nil(t, res)
+				require.Error(t, err)
+			} else {
+				require.NotNil(t, res)
+				require.NoError(t, err)
+			}
+		})
+	}
 }
