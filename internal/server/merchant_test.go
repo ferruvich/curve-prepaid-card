7d7e79057aa3a128fe2f7e442f497c6b4076ab6e
ferruvich <ferruvich@gmail.com>
2019-01-12 02:04:19 +0100

    testing user and merchant servers

--- internal/server/merchant_test.go
+++ internal/server/merchant_test.go
@@ -1,3 +1,69 @@
 package server
 
-// TODO
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ferruvich/curve-prepaid-card/internal/database"
+	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMerchant_Create(t *testing.T) {
+
+	t.Run("should run", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		mockMerchant := database.NewMockMerchant(controller)
+		mockMerchant.EXPECT().Write(gomock.Any()).Return(nil)
+
+		mockDB := database.NewMockDataBase(controller)
+		mockDB.EXPECT().Merchant().Return(mockMerchant)
+
+		server := &Service{
+			db: mockDB,
+		}
+
+		router := server.Routers()
+
+		testRequest := httptest.NewRequest(
+			http.MethodPost, "/merchant", nil,
+		)
+
+		testRecorder := httptest.NewRecorder()
+
+		router.ServeHTTP(testRecorder, testRequest)
+
+		require.Equal(t, http.StatusCreated, testRecorder.Code)
+	})
+
+	t.Run("should fail due to db error", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		mockCard := database.NewMockMerchant(controller)
+		mockCard.EXPECT().Write(gomock.Any()).Return(errors.New("error"))
+
+		mockDB := database.NewMockDataBase(controller)
+		mockDB.EXPECT().Merchant().Return(mockCard)
+
+		server := &Service{
+			db: mockDB,
+		}
+
+		router := server.Routers()
+
+		testRequest := httptest.NewRequest(
+			http.MethodPost, "/merchant", nil,
+		)
+
+		testRecorder := httptest.NewRecorder()
+
+		router.ServeHTTP(testRecorder, testRequest)
+
+		require.Equal(t, http.StatusInternalServerError, testRecorder.Code)
+	})
+}
--- internal/server/user_test.go
+++ internal/server/user_test.go
@@ -1,3 +1,69 @@
 package server
 
-// TODO
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ferruvich/curve-prepaid-card/internal/database"
+	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestUser_Create(t *testing.T) {
+
+	t.Run("should run", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		mockUser := database.NewMockUser(controller)
+		mockUser.EXPECT().Write(gomock.Any()).Return(nil)
+
+		mockDB := database.NewMockDataBase(controller)
+		mockDB.EXPECT().User().Return(mockUser)
+
+		server := &Service{
+			db: mockDB,
+		}
+
+		router := server.Routers()
+
+		testRequest := httptest.NewRequest(
+			http.MethodPost, "/user", nil,
+		)
+
+		testRecorder := httptest.NewRecorder()
+
+		router.ServeHTTP(testRecorder, testRequest)
+
+		require.Equal(t, http.StatusCreated, testRecorder.Code)
+	})
+
+	t.Run("should fail due to db error", func(t *testing.T) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		mockUser := database.NewMockUser(controller)
+		mockUser.EXPECT().Write(gomock.Any()).Return(errors.New("error"))
+
+		mockDB := database.NewMockDataBase(controller)
+		mockDB.EXPECT().User().Return(mockUser)
+
+		server := &Service{
+			db: mockDB,
+		}
+
+		router := server.Routers()
+
+		testRequest := httptest.NewRequest(
+			http.MethodPost, "/user", nil,
+		)
+
+		testRecorder := httptest.NewRecorder()
+
+		router.ServeHTTP(testRecorder, testRequest)
+
+		require.Equal(t, http.StatusInternalServerError, testRecorder.Code)
+	})
+}
